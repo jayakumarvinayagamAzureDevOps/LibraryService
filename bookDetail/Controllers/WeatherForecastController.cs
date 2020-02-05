8dc37401868e471239dda418f2158ac0546d37ae
jayakumarvinayagamAzureDevOps <jayakumarvinayagamdevice@outlook.com>
2020-02-05 07:26:42 +0530

    chennai added

--- bookDetail/Controllers/WeatherForecastController.cs
+++ bookDetail/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace bookDetail.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Chennai"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
